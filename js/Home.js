590e56a40b06a436e42d451733e1b3d67a018b37
SuRonald <35445753+SuRonald@users.noreply.github.com>
2021-06-20 22:00:36 +0700

    Update Home.js

--- js/Home.js
+++ js/Home.js
@@ -17,21 +17,3 @@ function openCity(evt, cityName){
 function adding(){
     alert("Added to Cart!");
 }
-
-var mybutton = document.getElementById("myBtn");
-
-window.onscroll = function(){scrollFunction()};
-
-function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        mybutton.style.display = "block";
-    }
-    else {
-        mybutton.style.display = "none";
-    }
-}
-
-function topFunction() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-}
